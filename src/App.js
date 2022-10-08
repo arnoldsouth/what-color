07d8734d62a4eec3d35c210986b1db6e38c1cab7
arnoldsouth <arnoldsouth@gmail.com>
2022-10-07 22:03:02 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const generateHexidecimal = () => {
+    const digits = [
+        "0",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+    ];
+
+    const color = new Array(6)
+        .fill("")
+        .map(() => digits[Math.floor(Math.random() * digits.length)])
+        .join("");
+
+    return `#${color}`;
+};
+
+const App = () => {
+    const [color, setColor] = useState("");
+    const [answers, setAnswers] = useState([]);
+    const [result, setResult] = useState();
+
+    const generateRandomColor = () => {
+        const actualColor = generateHexidecimal();
+        setColor(actualColor);
+        setAnswers(
+            // Randomizes the answer choice array
+            [actualColor, generateHexidecimal(), generateHexidecimal()].sort(
+                () => 0.5 - Math.random()
+            )
+        );
+    };
+
+    useEffect(() => {
+        generateRandomColor();
+        // Ensure dependency array is empty, otherwise the function will run every time the component rerenders
+    }, []);
+
+    const answerClickHandler = (answer) => {
+        if (answer === color) {
+            setResult(true);
+            generateRandomColor();
+        } else {
+            // Reselect colors if incorrect
+            setResult(false);
+        }
+    };
+
+    return (
+        <div className="App">
+            <div>
+                <h1>what color?</h1>
+                <p>try and guess the correct color</p>
+                <div
+                    className="guess"
+                    style={{ backgroundColor: color }}
+                ></div>
+
+                {answers.map((answer) => (
+                    <button
+                        onClick={() => answerClickHandler(answer)}
+                        className="button"
+                        key={answer}
+                    >
+                        {answer}
+                    </button>
+                ))}
+                {result === true && (
+                    <div className="correctAnswer">Correct Answer</div>
+                )}
+                {result === false && (
+                    <div className="incorrectAnswer">Incorrect Answer</div>
+                )}
+            </div>
+        </div>
+    );
+};
 
 export default App;
